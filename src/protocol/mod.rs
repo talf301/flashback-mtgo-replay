99a4305d01b9477bd3bf35e30845209b4257f780
Tal Friedman <talf301@gmail.com>
2026-02-15 19:20:47 -0800

    feat: add initial protocol decoder placeholder with event types

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,3 +1,5 @@
+pub mod decoder;
 pub mod raw_analyzer;
-
+pub use decoder::{GameEvent, DecodedEvent, decode_packet, decode_stream};
 pub use raw_analyzer::{PacketAnalysis, Pattern, analyze_packet, analyze_dump_file};
+
